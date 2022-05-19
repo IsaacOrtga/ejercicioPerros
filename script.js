b8c10a100cbd55b9d74fdc40a77ecc189a0b6dd3
IsaacOrtga <103757336+IsaacOrtga@users.noreply.github.com>
2022-05-19 16:31:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let cont = 1;
+let totalRazas = [];
 function perro() {
     fetch('https://dog.ceo/api/breeds/image/random')
         .then(res => res.json())
@@ -6,8 +7,21 @@ function perro() {
         .then(json => document.getElementById('random').src = json.message)
         .then(() => {
             let foto = document.getElementById('random').src
-            document.getElementById(`c${cont}`).src = foto
-            cont++
+            let separar = foto.split('/');
+            let raza = separar[4]
+            var condicion=totalRazas.includes(raza)           
+            
+            // console.log(raza)
+            // console.log(!condicion)
+            // totalRazas.push(raza)
+            if (!condicion) {
+                document.getElementById(`c${cont}`).src = foto
+                totalRazas.push(raza)
+                cont++
+            } else {
+                alert('La raza de perro está repetida')
+                totalRazas.push(raza)
+            }
+            console.log(totalRazas);
         })
 }
-
