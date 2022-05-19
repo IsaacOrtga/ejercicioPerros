6073ac937c9bde2e6a68cf36281f859c23ffbd30
Khenay <jurjomed@hotmail.com>
2022-05-19 14:21:11 +0200

    ya se quedan las imagenes

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
+let cont = 1;
 function perro() {
     fetch('https://dog.ceo/api/breeds/image/random')
         .then(res => res.json())
         .then(json => json)
-
-        .then(json => {
-            document.getElementById('random').src = json.message;
-            return json
-        })
-        .then(json => {
-            
-            
-            // document.getElementById('c1').src = json.message
-            // if((document.getElementById('c2').src == '') && (document.getElementById('c1').src != '')) {
-            //     document.getElementById('c3').src = json.message;
-            // }
-            for (let i = 2; i <= 20; i++) {
-                if ((document.getElementById(`c${i}`).src == '') && (document.getElementById(`c${i-1}`).src == '')) {
-                    document.getElementById(`c${i}`).src = json.message;
-                }
-
-            }
+        .then(json => document.getElementById('random').src = json.message)
+        .then(() => {
+            let foto = document.getElementById('random').src
+            document.getElementById(`c${cont}`).src = foto
+            cont++
         })
 }
+
