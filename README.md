55fe6e5dcb9bcec8bbcdc84a1dff0f1f1144b33f
Isaac Ortega <103757336+IsaacOrtga@users.noreply.github.com>
2023-05-25 16:34:19 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# ejercicioPerros
+# Consumo de API dogs.ceo
+
+### Enlace a API:
+
+[API DOGS](https://dog.ceo)
+
+### Hemos utilizado:
+
+- HTML5
+- CSS3
+  - GRID
+- Javascript
+- Chart.js
+
+<video width="auto" height="auto" controls>
+  <source src="./assets/presentation.mp4" type="video/mp4">
+</video>
+
+### Cómo funciona:
+
+- Se consume la API y devuelve en formato JSON para recoger la src de la imagen
+- Se pregunta si esa imagen ya existe en el DOM
+- Si no existe en el DOM, se carga la imagen
+- Si existe se introduce en un array y se contabiliza la raza, sacando un alert para señalar que el perro está repetido y no se mostrará en el DOM:
+```
+.then((json) => {
+      if (cont < 20) {
+        document.getElementById("random").src = json.message;
+      } else {
+        document.getElementById("random").src = json.message;
+        let foto = document.getElementById("random").src;
+        let separar = foto.split("/");
+        let raza = separar[4];
+        var condicion = totalRazas.includes(raza);
+        totalRazas.push(raza);
+        if (!condicion) {
+          labelsX.push(raza);
+          frecuencias[cont - 1] = 1;
+        } else {
+          let pos = labelsX.indexOf(raza);
+          console.log(pos);
+          frecuencias[pos]++;
+        }
+      }
+```
+
+
+- Si aparece la imagen de un gato, señalamos con el mensaje !Ups, se ha colado un gato!
+- Cada raza que se muestra se va guardadno en el Storage
+- Finalmente, se puede visualizar el total de razas mostradas en un gráfico, y el índice de cada una con [Chart.js](https://www.chartjs.org/)
+```
+.then(() => {
+      if (labelsX.length == 20) {
+        const labels = labelsX;
+        const data = {
+          labels: labels,
+          datasets: [
+            {
+              label: "Dog breeds",
+              backgroundColor: "teal",
+              borderColor: "goldenrod",
+              data: frecuencias,
+            },
+          ],
+        };
+        const config = {
+          type: "line",
+          data: data,
+          options: {},
+        };
+```
+- El array con todas las razas cargadas se muestra en consola.
--- script.js
+++ script.js
@@ -2,104 +2,101 @@ let cont = 1;
 // let contX=0;
 let totalRazas = [];
 const labelsX = [];
-const frecuencias = [,,,,,,,,,,,,,,,,,,,,];
+const frecuencias = [, , , , , , , , , , , , , , , , , , , ,];
 function perro() {
-    fetch('https://dog.ceo/api/breeds/image/random')
-        .then(res => res.json())
-        .then(json => json)
-        .then(json => {
-            if (cont < 20) {
-                document.getElementById('random').src = json.message
-            } else {
-                document.getElementById('random').src = json.message
-                let foto = document.getElementById('random').src
-                let separar = foto.split('/');
-                let raza = separar[4]
-                var condicion = totalRazas.includes(raza)
-                totalRazas.push(raza)
-                if (!condicion) {
-                    labelsX.push(raza)
-                    frecuencias[cont-1]=1
-                    // contX++    
-                    
-                } else {
-                    let pos=labelsX.indexOf(raza)
-                    console.log(pos)
-                    frecuencias[pos]++
-                    
-                }
-            }
-            return json
-        })
-        .then(() => {
-            if (cont < 20) {
-                let foto = document.getElementById('random').src
-                let separar = foto.split('/');
-                let raza = separar[4]
-                var condicion = totalRazas.includes(raza)
-                totalRazas.push(raza)
-                if (!condicion) {
-                    document.getElementById(`c${cont}`).src = foto
-                    labelsX.push(raza)
-                    document.getElementById('showBreed').innerText = `¡Has encontrado un ${raza}!`
-                    frecuencias[cont-1]=1
-                    cont++
-                    // contX++
-                } else {
-                    alert(`La raza de perro ${raza} está repetida`)
-                    let pos=labelsX.indexOf(raza)
-                    console.log(pos)
-                    frecuencias[pos]++
-                    
-                }
-                localStorage.setItem("razas", raza);
-                razas=localStorage.getItem("arrayRazas"); //RAZAS GUARDADAS EN EL STORAGE Y PINTADAS EN CONSOLE.LOG
-            } else {
-                fetch('https://api.thecatapi.com/v1/images/search')
-                    .then(res => res.json())
-                    .then(json => json)
-                    .then(json => {
-                        let data = json[0]
-                        document.getElementById('random').src = data.url
-                    })
-                    .then(() => {
-                        let foto = document.getElementById('random').src
-                        document.getElementsByClassName('boton')[0].innerText = '¡Miau!'
-                        document.getElementById(`c${cont}`).src = foto
-                        document.getElementById('showBreed').innerText = '¡Ups! se ha colado un gato'
-                        document.getElementById('titulo').innerText = 'Gatos'
-                        document.getElementById('favIcon').href = 'http://4.bp.blogspot.com/-g3tGAC5psHg/UPHMfH-wMQI/AAAAAAAAAY4/VmD17lwrIww/s1600/BCPicon123.jpg'
-                    })
-            }
-            console.log(totalRazas);
-            console.log(labelsX)
-            // console.log(contX)
-            console.log(frecuencias)
-            
-objeto = JSON.stringify(razas);
-console.log(objeto);
-        })
-        .then(()=> {
-            if (labelsX.length == 20) {
-                const labels = labelsX;
-                const data = {
-                    labels: labels,
-                    datasets: [{
-                        label: 'Dog breeds',
-                        backgroundColor: 'teal',
-                        borderColor: 'goldenrod',
-                        data: frecuencias,
-                    }]
-                };
-                const config = {
-                    type: 'line',
-                    data: data,
-                    options: {}
-                };
-                const myChart = new Chart(
-                    document.getElementById('myChart'),
-                    config
-                );
-            }
-        })
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => res.json())
+    .then((json) => json)
+    .then((json) => {
+      if (cont < 20) {
+        document.getElementById("random").src = json.message;
+      } else {
+        document.getElementById("random").src = json.message;
+        let foto = document.getElementById("random").src;
+        let separar = foto.split("/");
+        let raza = separar[4];
+        var condicion = totalRazas.includes(raza);
+        totalRazas.push(raza);
+        if (!condicion) {
+          labelsX.push(raza);
+          frecuencias[cont - 1] = 1;
+        } else {
+          let pos = labelsX.indexOf(raza);
+          console.log(pos);
+          frecuencias[pos]++;
+        }
+      }
+      return json;
+    })
+    .then(() => {
+      if (cont < 20) {
+        let foto = document.getElementById("random").src;
+        let separar = foto.split("/");
+        let raza = separar[4];
+        var condicion = totalRazas.includes(raza);
+        totalRazas.push(raza);
+        if (!condicion) {
+          document.getElementById(`c${cont}`).src = foto;
+          labelsX.push(raza);
+          document.getElementById(
+            "showBreed"
+          ).innerText = `¡Has encontrado un ${raza}!`;
+          frecuencias[cont - 1] = 1;
+          cont++;
+        } else {
+          alert(`La raza de perro ${raza} está repetida`);
+          let pos = labelsX.indexOf(raza);
+          console.log(pos);
+          frecuencias[pos]++;
+        }
+        localStorage.setItem("razas", raza);
+        razas = localStorage.getItem("arrayRazas"); //RAZAS GUARDADAS EN EL STORAGE Y PINTADAS EN CONSOLE.LOG
+      } else {
+        fetch("https://api.thecatapi.com/v1/images/search")
+          .then((res) => res.json())
+          .then((json) => json)
+          .then((json) => {
+            let data = json[0];
+            document.getElementById("random").src = data.url;
+          })
+          .then(() => {
+            let foto = document.getElementById("random").src;
+            document.getElementsByClassName("boton")[0].innerText = "¡Miau!";
+            document.getElementById(`c${cont}`).src = foto;
+            document.getElementById("showBreed").innerText =
+              "¡Ups! se ha colado un gato";
+            document.getElementById("titulo").innerText = "Gatos";
+            document.getElementById("favIcon").href =
+              "http://4.bp.blogspot.com/-g3tGAC5psHg/UPHMfH-wMQI/AAAAAAAAAY4/VmD17lwrIww/s1600/BCPicon123.jpg";
+          });
+      }
+      console.log(totalRazas);
+      console.log(labelsX);
+      console.log(frecuencias);
+
+      objeto = JSON.stringify(razas);
+      console.log(objeto);
+    })
+    .then(() => {
+      if (labelsX.length == 20) {
+        const labels = labelsX;
+        const data = {
+          labels: labels,
+          datasets: [
+            {
+              label: "Dog breeds",
+              backgroundColor: "teal",
+              borderColor: "goldenrod",
+              data: frecuencias,
+            },
+          ],
+        };
+        const config = {
+          type: "line",
+          data: data,
+          options: {},
+        };
+        const myChart = new Chart(document.getElementById("myChart"), config);
+      }
+    });
 }
